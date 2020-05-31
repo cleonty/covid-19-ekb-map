38d78d831fec7947f91c54c468670d4bbdeca9d3
Leonty Chudinov <leonty@inbox.ru>
2020-05-31 21:38:10 +0500

    Load data

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import https from 'https';
+import { IncomingMessage } from 'http';
 const PORT = process.env.PORT || 5000;
 
 express()
   .use('/', express.static('public'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+  .get('/data/COVID.json', (req: express.Request, res: express.Response) => {
+    https.get('https://www.gorses.na4u.ru/data/COVID.json', { rejectUnauthorized: false }, (response: IncomingMessage) => {
+      response.pipe(res, { end: true });
+    });
+  })
+  .listen(PORT, () => console.log(`Listening on ${PORT}`));
