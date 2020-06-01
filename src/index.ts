73a61abc0ae0d50f515c5f7116a2c0fd5b313a83
Leonty Chudinov <leonty@inbox.ru>
2020-06-01 18:21:56 +0500

    Save data on filesystem

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,17 @@
 import express from 'express';
 import https from 'https';
 import { IncomingMessage } from 'http';
+import * as fs from 'fs';
 const PORT = process.env.PORT || 5000;
 
 express()
   .use('/', express.static('public'))
   .get('/data/COVID.json', (req: express.Request, res: express.Response) => {
     https.get('https://www.gorses.na4u.ru/data/COVID.json', { rejectUnauthorized: false }, (response: IncomingMessage) => {
+      const date = (new Date()).toISOString().substr(0, 10);
+      const file = fs.createWriteStream(`data/COVID-${date}.txt`);  
       response.pipe(res, { end: true });
+      response.pipe(file, { end: true });
     });
   })
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
