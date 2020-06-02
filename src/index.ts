d47925535cd47d866e6c04d2e5e47afc59ad81bc
Leonty Chudinov <leonty@inbox.ru>
2020-06-02 14:25:56 +0500

    Add a service for yesterday's data

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,14 @@ express()
       response.pipe(file, { end: true });
     });
   })
+  .get('/data/COVID-yesterday.json', (req: express.Request, res: express.Response) => {
+    const date = new Date();
+    date.setDate(date.getDate() - 1);
+    const yesterday = date.toISOString().substr(0, 10);
+      const file = fs.createReadStream(`./data/COVID-${yesterday}.json`);
+      file.on('error', (e) => res.status(404).json({error: e.message}));
+      file.pipe(res, { end: true });
+  })
   .get('/env', (req: express.Request, res: express.Response) => {
     const data = { env: process.env, cwd: process.cwd() };
     res.json(data);
